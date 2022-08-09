ff9d3c59cfaef5f345bb693d12a4c6339f278041
Glenn Sarti <glennsarti@users.noreply.github.com>
2022-08-09 15:00:03 +0800

    Reread main configuration for each product
    Previously it was thought that the main Viper configuration would be refreshed
    after each merge however this turned out to be false.  This commit updates
    the builder to refresh the main viper instance before being merged with the
    product configuration.

--- builder/main.go
+++ builder/main.go
@@ -66,6 +66,13 @@ func (c *BuildCommand) Synopsis() string {
 	return "Builds syntax files"
 }
 
+func (c *BuildCommand) newViper(filepath string) (*viper.Viper, error) {
+	v := viper.New()
+	v.SetConfigFile(filepath)
+	err := v.ReadInConfig()
+	return v, err
+}
+
 func (c *BuildCommand) Run(args []string) int {
 	// This currently uses the cwd to find all the files used here
 	// This can be improved in the future to accept User input or use some kind of
@@ -77,40 +84,36 @@ func (c *BuildCommand) Run(args []string) int {
 
 	// The _main.yml file is where all rules in the Patterns and the Repository are defined
 	// Each product file provides specific overrides for rules defined in _main.yml
-	// Here we build a main Viper instance to store the config we will merge in product specific maps to
 	mainFile := filepath.Join(wd, "../src/_main.yml")
-	c.Ui.Info(fmt.Sprintf("Pulling in main template: %s", mainFile))
-	mainViper := viper.New()
-	mainViper.SetConfigFile(mainFile)
-	if err := mainViper.ReadInConfig(); err == nil {
-		c.Ui.Info(fmt.Sprintf("Merging config file: %v", mainViper.ConfigFileUsed()))
-	} else {
-		c.Ui.Error(fmt.Sprintf("Error reading %v: %v", mainViper.ConfigFileUsed(), err))
-		return 1
-	}
 
 	var result *multierror.Error
 	// For each product defined, read the yml and merge into the main Viper instance
 	products := []string{"hcl", "terraform"}
 	for _, product := range products {
 		c.Ui.Info(fmt.Sprintf("Evaluating %s", product))
+
+		// Here we build a main Viper instance to store the config we will merge in product specific maps.
+		// Note that the main viper configuration needs to be re-read for each product and errors here
+		// are terminal
+		mainViper, err := c.newViper(mainFile)
+		if err != nil {
+			c.Ui.Error(fmt.Sprintf("Error reading %v: %v", mainViper.ConfigFileUsed(), err.Error()))
+			result = multierror.Append(result, err)
+			return 1
+		}
+
 		productFile := filepath.Join(wd, fmt.Sprintf("../src/%s.yml", product))
 		c.Ui.Info(fmt.Sprintf("Processing: %s", productFile))
 
 		// Create a product Viper instance that reads each product yml for rules
-		productV := viper.New()
-		productV.SetConfigFile(productFile)
-		if err := productV.ReadInConfig(); err == nil {
-			c.Ui.Info(fmt.Sprintf("Merging config file: %v", productV.ConfigFileUsed()))
-		} else {
+		productV, err := c.newViper(productFile)
+		if err != nil {
 			c.Ui.Error(fmt.Sprintf("Error reading %v: %v", productV.ConfigFileUsed(), err.Error()))
 			result = multierror.Append(result, err)
 			continue
 		}
+		c.Ui.Info(fmt.Sprintf("Merging config file: %v", productV.ConfigFileUsed()))
 
-		// merge the product Viper map into the main Viper instance
-		// This overrides anything already loaded so each product can replace existing
-		// rules and/or provide new ones
 		if err := mainViper.MergeConfigMap(productV.AllSettings()); err != nil {
 			c.Ui.Error(fmt.Sprintf("Unable to merge values from %v: %v", productV.ConfigFileUsed(), err.Error()))
 			result = multierror.Append(result, err)
@@ -120,7 +123,7 @@ func (c *BuildCommand) Run(args []string) int {
 		// Export the merged map to a struct so we can write to file
 		c.Ui.Info(fmt.Sprintf("Building %s", product))
 		var config TextMateGrammar
-		err := mainViper.Unmarshal(&config)
+		err = mainViper.Unmarshal(&config)
 		if err != nil {
 			c.Ui.Error(fmt.Sprintf("Unable to merge values from %s: %s", productV.ConfigFileUsed(), err.Error()))
 			result = multierror.Append(result, err)
